3716791463add68ee2b30545960e02171e2f931c
SeukWon Kang <kasworld@gmail.com>
2015-02-15 10:42:26 +0900

    first commit

---
+++ abs.go
@@ -0,0 +1,56 @@
+package abs
+
+func SignAbsi(n int) (int, int) {
+	if n > 0 {
+		return 1, n
+	} else if n < 0 {
+		return -1, -n
+	} else {
+		return 0, 0
+	}
+}
+func SignAbsf(n float64) (float64, float64) {
+	if n > 0 {
+		return 1, n
+	} else if n < 0 {
+		return -1, -n
+	} else {
+		return 0, 0
+	}
+}
+func Signi(n int) (int, int) {
+	if n > 0 {
+		return 1, n
+	} else if n < 0 {
+		return -1, -n
+	} else {
+		return 0, 0
+	}
+}
+func Signf(n float64) (float64, float64) {
+	if n > 0 {
+		return 1, n
+	} else if n < 0 {
+		return -1, -n
+	} else {
+		return 0, 0
+	}
+}
+func Absi(n int) (int, int) {
+	if n > 0 {
+		return 1, n
+	} else if n < 0 {
+		return -1, -n
+	} else {
+		return 0, 0
+	}
+}
+func Absf(n float64) (float64, float64) {
+	if n > 0 {
+		return 1, n
+	} else if n < 0 {
+		return -1, -n
+	} else {
+		return 0, 0
+	}
+}
